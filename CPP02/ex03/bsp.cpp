29389bfca8c922782c0588c43d3a6a9198f866d1
ma-gabriel <gabi.eymat@gmail.com>
2024-12-13 02:07:30 +0100

    OK

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -2,17 +2,18 @@
 #include "Point.hpp"
 #include <iostream>
 
-static Fixed find_area_triangle(Point const &a, Point const &b, Point const &c);
+static inline Fixed cross_product(Point first, Point second, Point aimed);
 
 bool bsp( Point const a, Point const b, Point const c, Point const point){
-    return 
-    find_area_triangle(a, b, c) == 
-    find_area_triangle(a, b, point) +
-    find_area_triangle(b, c, point) +
-    find_area_triangle(c, a, point);
+    Fixed side1 = cross_product(a, b, point);
+    Fixed side2 = cross_product(b, c, point);
+    Fixed side3 = cross_product(c, a, point);
+	return (side1 < 0 && side2 < 0 && side3 < 0) || (side1 > 0 && side2 > 0 && side3 > 0);
 }
 
-static Fixed find_area_triangle(Point const &a, Point const &b, Point const &c)
+
+static inline Fixed cross_product(Point first, Point second, Point aimed)
 {
-    return Fixed(0.5) * Fixed(a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY()) + c.getX() * (a.getY() - b.getY())).abs();
+	return (first.getX() - second.getX()) * (first.getY() - aimed.getY())
+	 - (first.getY() - second.getY()) * (first.getX() - aimed.getX());
 }
--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -3,14 +3,26 @@
 #include "Point.hpp"
 
 int main( void ) {
-    Point a(0, 0);
-    Point b(20, 0);
-    Point c(10, 30);
-    Point point(-1, 0);
+    Point a(1, 1);
+    Point b(5, 1);
+    Point c(3, 4);
 
-    std::cout << (bsp(a, b, c, point) ? "true" : "false") << std::endl;
+	// on the side of the triangle is false, according to subject
+    std::cout << (bsp(a, b, c, Point(3, 1)) ? "true" : "false") << std::endl;
 
-    std::cout << (bsp(a, b, c, point) ? "true" : "false") << std::endl;
+    std::cout << (bsp(a, b, c, Point(2, 2)) ? "true" : "false") << std::endl;
+
+    std::cout << (bsp(a, b, c, Point(4, 2)) ? "true" : "false") << std::endl;
+
+    std::cout << (bsp(a, b, c, Point(3, 3)) ? "true" : "false") << std::endl;
+
+    std::cout << (bsp(a, b, c, Point(6, 5)) ? "true" : "false") << std::endl;
+
+    std::cout << (bsp(a, b, c, Point(2, 3)) ? "true" : "false") << std::endl;
+
+    std::cout << (bsp(a, b, c, Point(1, 2)) ? "true" : "false") << std::endl;
+
+    std::cout << (bsp(a, b, c, Point(-1, 0)) ? "true" : "false") << std::endl;
     
     return 0;
 }
