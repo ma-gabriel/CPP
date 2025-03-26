e66862c879b8286dc159fbfce351e6108cd16366
ma-gabriel <gabi.eymat@gmail.com>
2025-03-26 08:38:56 +0100

    for almost nothing

--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -4,9 +4,14 @@
 # include <string>
 
 class ScalarConverter {
+	private:
+		ScalarConverter();
+		ScalarConverter(ScalarConverter const &);
+		ScalarConverter &operator=(const ScalarConverter &);
+
+		virtual ~ScalarConverter() = 0;
 	public:
 		static void convert(std::string);
-		virtual ~ScalarConverter() = 0;
 };
 
 #endif
