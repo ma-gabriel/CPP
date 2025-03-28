03101e94df6f47e1b4fd4823209f20af70353a52
ma-gabriel <gabi.eymat@gmail.com>
2025-03-28 12:16:49 +0100

    up to date

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -19,72 +19,72 @@ ScalarConverter::ScalarConverter(ScalarConverter const &)
 ScalarConverter::~ScalarConverter()
 {}
 
-static void convert_c(char *);
-static void convert_i(char *);
-static void convert_d(std::string);
-static void convert_f(std::string);
+static double findValue(std::string);
+static void convert_c(double);
+static void convert_i(double);
+static void convert_d(double);
+static void convert_f(double);
 
 
 
 void ScalarConverter::convert(std::string s)
 {
-    convert_c(const_cast<char *>(s.c_str()));
-    convert_i(const_cast<char *>(s.c_str()));
-    convert_f(s);
-    convert_d(s);
+    double d = findValue(s);
+    convert_c(d);
+    convert_i(d);
+    std::cout << std::fixed;
+    std::cout << std::setprecision(1);
+    convert_f(d);
+    convert_d(d);
 }
 
-
-static void convert_i(char *s)
+static void convert_c(double d)
 {
-    char *end;
-    int i = strtol(s, &end, 10);
-    if (s == end)
-        std::cout << "int: impossible\n";
-    else 
-        std::cout << "int: " << i << "\n";
-}
 
-static void convert_c(char *s)
-{
-    char *end;
-    char c = strtol(s, &end, 10);
-    if (s == end)
+    if (d != d)
         std::cout << "char: impossible\n";
-    else if (std::isprint(c))
-        std::cout << "char: '" << c << "'\n";
+    else if (d > std::numeric_limits<char>::max())
+        std::cout << "char: overflowed\n";
+    else if (d < std::numeric_limits<char>::min())
+        std::cout << "char: underflowed\n";
+    else if (std::isprint(static_cast<char>(d)))
+        std::cout << "char: '" << static_cast<char>(d) << "'\n";
     else
-        std::cout << "char: non displayable\n";
+        std::cout << "int: Non displayable\n";
 }
 
-static void convert_f(std::string s)
+static void convert_i(double d)
 {
-    float f;
-    std::cout.precision(1);
-    std::cout << std::fixed;
-    errno = 0;
-    if (std::stringstream(s) >> f)
-        std::cout << "float: " << f << "f\n";
-    else if (errno == ERANGE && f == std::numeric_limits<float>::max())
-        std::cout << "float: +inff\n";
-    else if (errno == ERANGE && f == -std::numeric_limits<float>::max())
-        std::cout << "float: -inff\n";
+    if (d != d)
+        std::cout << "int: impossible\n";
+    else if (d > std::numeric_limits<int>::max())
+        std::cout << "int: overflowed\n";
+    else if (d < std::numeric_limits<int>::min())
+        std::cout << "int: underflowed\n";
     else
-        std::cout << "float: nanf\n";
+        std::cout << "int: " << static_cast<int>(d) << "\n";
 }
 
-static void convert_d(std::string s)
-{   
+static void convert_f(double d)
+{
+    float f = static_cast<float>(d);
+    std::cout << "float: " << f << "f\n";
+}
+
+static void convert_d(double d)
+{
+    std::cout << "double: " << d << std::endl;
+}
+
+static double findValue(std::string s)
+{
     double d;
-    std::cout.precision(1);
-    std::cout << std::fixed;
     errno = 0;
     if (std::stringstream(s) >> d)
-        std::cout << "double: " << d << std::endl;
-    else if (errno == ERANGE && d == std::numeric_limits<double>::max())
-        std::cout << "double: +inf" << std::endl;
-    else if (errno == ERANGE && d == -std::numeric_limits<double>::max())
-        std::cout << "double: -inf" << std::endl;
-    else
-        std::cout << "double: nan" << std::endl;
+        return d;
+    if (errno == ERANGE && d == std::numeric_limits<double>::max())
+        return std::numeric_limits<double>::infinity();
+    if (errno == ERANGE && d == -std::numeric_limits<double>::max())
+        return -std::numeric_limits<double>::infinity();
+    return std::numeric_limits<double>::quiet_NaN();
 }
