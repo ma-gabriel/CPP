b338667b30ef820d0f4caadea120b090c934679b
ma-gabriel <gabi.eymat@gmail.com>
2025-06-03 18:45:13 +0200

    OK

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -11,10 +11,17 @@ class MutantStack : public std::stack<T>
 		MutantStack() {};
 		MutantStack(const MutantStack &ref) {this->c = ref.c;}
 		~MutantStack() {};
-		typedef typename std::stack<T>::container_type::iterator iterator;
 		MutantStack &operator=(const MutantStack &ref) {this->c = ref.c;};
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
 		iterator begin() {return this->c.begin();};
 		iterator end() {return this->c.end();};
+		iterator begin() const {return this->c.begin();};
+		iterator end() const {return this->c.end();};
+		reverse_iterator rbegin() {return this->c.rbegin();};
+		reverse_iterator rend() {return this->c.rend();};
+		reverse_iterator rbegin() const {return this->c.rbegin();};
+		reverse_iterator rend() const {return this->c.rend();};
 };
 
 #endif
