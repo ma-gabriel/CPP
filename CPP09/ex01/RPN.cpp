0e65b068d7bf877626f64d2f50556a4196ab5b53
Gabriel Eymat <geymat@z4r11p1.42lyon.fr>
2025-07-01 05:53:28 +0200

    OK

--- CPP09/ex01/RPN.cpp
+++ CPP09/ex01/RPN.cpp
@@ -13,47 +13,58 @@ RPN &RPN::operator=(const RPN &){ return *this;}
 int RPN::process(char *argv)
 {
     std::stack<int> data;
-    int res;
     while (argv[0] == ' ')
         argv++;
     if (std::string("+-*/").find(argv[0]) != std::string::npos)
         throw std::string("need to start with a number");
-    res = argv[0] - '0';
-    for (size_t i = 1; argv[i]; i++)
+    for (size_t i = 0; argv[i]; i++)
     {
+        int res;
         if (std::string("0123456789 +-*/").find(argv[i]) == std::string::npos)
             throw std::string("bad format, we only take space, numbers, and +-/*");
         switch (argv[i])
         {
             case '+':
-                if (data.empty())
+                if (data.size() < 2)
                     throw std::string("tried doing the operator + with not enough numbers before");
+                res = data.top();
+                data.pop();
                 if (__builtin_sadd_overflow (res, data.top(), &res))
                     throw std::string("overflow of the int");
                 data.pop();
+                data.push(res);
                 break;
 
             case '-':
-                if (data.empty())
+                if (data.size() < 2)
                     throw std::string("tried doing the operator - with not enough numbers before");
-                if (__builtin_ssub_overflow (res, data.top(), &res))
+                res = data.top();
+                data.pop();
+                if (__builtin_ssub_overflow (data.top(), res, &res))
                     throw std::string("overflow of the int");
                 data.pop();
+                data.push(res);
                 break;
             
             case '*':
-                if (data.empty())
+                if (data.size() < 2)
                     throw std::string("tried doing the operator * with not enough numbers before");
+                res = data.top();
+                data.pop();
                 if (__builtin_smul_overflow (res, data.top(), &res))
                     throw std::string("overflow of the int");
                 data.pop();
+                data.push(res);
                 break;
             
             case '/':
-                if (data.empty())
+                if (data.size() < 2)
                     throw std::string("tried doing the operator - with not enough numbers before");
-                res = res / data.top();
+                res = data.top();
                 data.pop();
+                res = data.top() / res;
+                data.pop();
+                data.push(res);
                 break;
             
             case ' ':
@@ -63,8 +74,8 @@ int RPN::process(char *argv)
                 break;
         }
     }
-    if (!data.empty())
+    if (data.size() != 1)
         throw std::string("too many numbers");
-    return res;
+    return data.top();
 
 }
